b277dab5b3c192e71b59a7aa0c0eeb2f5bc46880
plabon chowdhury <plabonchowdhury9@gmail.comgit config --global user.name Plabssgit config --global user.email plabonchowdhury9@gmail.com>
2022-08-11 03:34:31 +0600

    bonus

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //problem 1:
 
 function radianToDegree(x) {
+    if (typeof x != 'number') {
+        return "Please input numeric value!"
+    }
     return (180 / Math.PI) * x;
 }
 /*
@@ -11,9 +14,12 @@ console.log(ans);
 //problem 2:
 
 function isJavaScriptFile(str) {
+    if (typeof str != 'string') {
+        return "Please input a string!"
+    }
     let size_of_str = str.length;
     let cnt = 0;
-    for (var i = 0; i < size_of_str; i++) {
+    for (let i = 0; i < size_of_str; i++) {
         if (str[i] == '.') {
             cnt++;
         }
@@ -23,13 +29,16 @@ function isJavaScriptFile(str) {
     }
     return true;
 }
-/*
-console.log(isJavaScriptFile("anikSarkar.js"));
-*/
+
+console.log(isJavaScriptFile("index.js"));
+
 
 //problem 3:
 
 function oilPrice(diesel, petrol, octane) {
+    if (typeof diesel != 'number' || typeof petrol != 'number' || typeof octane != 'number') {
+        return "Please input numeric value!"
+    }
     return (diesel * 114) + (petrol * 130) + (octane * 135);
 }
 /*
@@ -39,25 +48,31 @@ console.log(oilPrice(3, 2, 3));
 //problem 4:
 
 function publicBusFare(x) {
+    if (typeof x != 'number') {
+        return "Please input numeric value!"
+    }
     //remain after bus
-    var micro = x % 50;
+    let micro = x % 50;
     //remain after micro
-    var public = micro % 11;
+    let public = micro % 11;
 
     return public * 250;
 }
 /*
-console.log(publicBusFare(9))
+console.log(publicBusFare(45))
 */
 
 //problem 5:
 
 function isBestFriend(first, second) {
+    if (typeof first != 'object' || typeof second != 'object') {
+        return "Please input object!"
+    }
     if (first.name != second.friend || first.friend != second.name) {
         return false;
     }
     return true;
 }
 /*
-console.log(isBestFriend({ name: "anik", friend: "dip" }, { name: "dip", friend: "ani" }));
+console.log(isBestFriend({ name: "anik", friend: "dip" }, { name: "dip", friend: "anik" }));
 */
